78eca0d7f491c27d5c16762335351f743641038f
aakashv100 <vardhan.aakash1@gmail.com>
2024-04-05 16:34:40 +0400

    Update main.py with LR finder and OneCycleLR scheduler

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau, OneCycleLR
 from setup_cifar10 import setup_cifar10
 import utils
 from torch_lr_finder import LRFinder
+import torch.nn as nn
 
 def main(config):
     """
@@ -16,19 +17,41 @@ def main(config):
     Returns:
         tuple: A tuple containing the trained model, test data loader, and a list of learning rates.
     """
+    criterion = nn.CrossEntropyLoss()
     train_data, test_data, train_loader, test_loader = setup_cifar10(config)
     model = CustomResNet(config).to(config["device"])
     model_summary(model, input_size=(3, 32, 32))
     optimizer = adam_optimizer(model, config)
     scheduler = StepLR(optimizer, step_size=config["step_size"], gamma=0.5)
     lr_plateau = ReduceLROnPlateau(optimizer, mode="min", patience=5, verbose=True)
+    lr_finder = LRFinder(model, optimizer, criterion, device="cuda:0")
+    lr_finder.range_test(train_loader, end_lr=10, num_iter=100, step_mode="exp")
+    _, max_lr = lr_finder.plot()  # to inspect the loss-learning rate graph
+    lr_finder.reset()
+    config["max_lr"] = max_lr
+    ocp_scheduler = OneCycleLR(
+        optimizer,
+        max_lr=config["max_lr"],
+        steps_per_epoch=len(train_loader),
+        epochs=config["epochs"],
+        pct_start=5 / config["epochs"],
+        div_factor=100,
+        three_phase=False,
+        final_div_factor=100,
+        anneal_strategy="linear",
+    )
     lr = []
     for epoch in range(1, config["epochs"] + 1):
         print("EPOCH:", epoch)
-        train(model, config["device"], train_loader, optimizer, epoch)
+        train(model, config["device"], train_loader, optimizer, criterion, epoch)
         test_loss = test(model, config["device"], test_loader)
         if config["lr_scheduler"] == "step_lr":
             scheduler.step()
+            lr.append(optimizer.param_groups[0]["lr"])
+            print("Learning rate:", optimizer.param_groups[0]["lr"])
+
+        elif config["lr_scheduler"] == "one_cycle":
+            ocp_scheduler.step()
             lr.append(optimizer.param_groups[0]["lr"])
             print("Learning rate:", optimizer.param_groups[0]["lr"])
         elif config["lr_scheduler"] == "plateau":
